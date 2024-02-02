be9a5eb3b269ade19375c51c3b4a647a59faf9df
Saba Noman <saba.noman5@gmail.com>
2024-02-02 20:13:08 +0500

    first commit

---
+++ shop/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+# class Category():
+#   categoryId = models.CharField(max_length=255)
+  
+# class Product(models.Model):
+#   name = models.CharField(max_length=255)
+#   price = models.IntegerField()
+#   image = models.ImageField(upload_to='path/to/upload/directory/')
+#   categoryId = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+#   def __str__(self):
+#     return self.name
+
+# class User():
+
+
+# class OrderDetails():
+  
+# class Inventory():
