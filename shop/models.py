785a3b377fe23143190701210f19e7e2dbd601e0
Saba Noman <saba.noman5@gmail.com>
2024-02-04 21:22:30 +0500

    third commit

--- shop/models.py
+++ shop/models.py
@@ -21,8 +21,11 @@ class Customer(models.Model):
 class Product(models.Model):
   name = models.CharField(max_length=255)
   price = models.DecimalField(default=0, decimal_places=2, max_digits=10)
+  ingredients = models.CharField(max_length=100, default='Vanilla')
   description = models.CharField(max_length=250, default='', blank='True')
-  image = models.ImageField(upload_to='uploads/products')
+  image1 = models.ImageField(upload_to='uploads/products')
+  image2 = models.ImageField(upload_to='uploads/products', blank='True', null='True')
+  image3 = models.ImageField(upload_to='uploads/products', blank='True', null='True')
   category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
 
   def __str__(self):
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Product
 
 def homepage(request):
     return render(request, 'candlehome.html')
 
 def shop_all(request):
-    return render(request, 'shopAll.html')
+    products = Product.objects.all()
+    return render(request, 'shopAll.html', {'products': products})
